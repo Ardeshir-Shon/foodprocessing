a5706335713a19270cb54b73b2d22b2f9a720b36
Ardeshir <ad.ardeshir@gmail.com>
2022-12-18 16:37:12 -0800

    extractor file contains data extrctor functions - created

---
+++ extractor.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def generate_material_and_jobs(file_production_name="Actual production.XLSX", 
+    file_material_name="Routing Timings.XLSX"):
+    # df = pd.read_csv('data.csv')
+    # return df
+    print(file_production_name, file_material_name)
+    print("Extracted data from csv file")
