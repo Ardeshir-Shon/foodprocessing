2308531b4227d8e65cdef50f8e9042b28cad0aa0
Ardeshir <ad.ardeshir@gmail.com>
2022-12-25 16:12:22 -0800

    finalized material and production info generation.

--- extractor.py
+++ extractor.py
@@ -9,17 +9,13 @@ def generate_material_and_jobs(file_production_name="Actual production.XLSX",
     # Filter the production file
     production = production[["Order","Material Number","MRP controller","Basic start date"]]
     production.rename(columns={"Basic start date": "start_date"}, inplace=True)
+    production.rename(columns={"MRP controller": "machine_number"}, inplace=True)
 
     # Read the material file
     material = pd.read_excel("data/"+file_material_name)
     # Filter the material file
     material = material[["Material", "Operation short text", "StdVal1","StdVal2"]]
     material = material[material["Operation short text"] == "Phase 5 Packing"]
-    
-    print(production.head())
-    print(material.head())
-    print(production.dtypes)
-    print(material.dtypes)
 
     times = pd.to_datetime(production.start_date)
     
@@ -28,25 +24,17 @@ def generate_material_and_jobs(file_production_name="Actual production.XLSX",
     production["day"] = times.dt.day
     production["dayofweek"] = times.dt.dayofweek
     production["dayofyear"] = times.dt.dayofyear
-    production["starthour"] = (production.dayofyear-1) * 24
+    production["starthour"] = (production.dayofyear-1) * 24 # start hour indexed in year
     production["end_date"] = times + pd.to_timedelta(6-times.dt.dayofweek, unit="d")
     production["end_dayofyear"] = pd.to_datetime(production.end_date).dt.dayofyear
-    production["endhour"] = production.end_dayofyear * 24
-    
-
-    print(production.head())
-    print(production.tail())
-
-    time_min = min(times)
-    time_max = max(times)
-
-    production_plan = {}
-    
-    # for index, row in production.iterrows():
-    #     production_plan[row["Material"]] = row["Quantity"]
+    production["endhour"] = production.end_dayofyear * 24 # end hour indexed in year
     
     material_info = {} 
-    print(file_production_name, file_material_name)
-    print("Extracted data from csv file")
 
-    return production_plan, material_info
+    for index, row in material.iterrows():
+        material_info[row["Material"]] = max(row["StdVal1"],row["StdVal2"])
+    
+    # print(file_production_name, file_material_name)
+    print("Extracted data from csv file ...")
+
+    return production, material_info
